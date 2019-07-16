9e8cc078f1831000586ce78a633a0d8e9f8f46b9
Shoumorup <mukho.shoumorup1564@gmail.com>
2019-07-16 12:05:56 +0000

    Books,cars,bikes webapp

---
+++ webapp/src/main/java/com/visa/training/web/ShoppingCartServlet.java
@@ -0,0 +1,69 @@
+package com.visa.training.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Stream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet({"/addbooks", "/addcars", "/addbikes"})
+
+public class ShoppingCartServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		HttpSession session = request.getSession();
+		if(uri.endsWith("addbooks"))
+		{
+			String[] selectedBooks = request.getParameterValues("Book");
+			//Storing in Session as required in future.
+			session.setAttribute("selectedBooks", selectedBooks);
+			//Forward request to cars
+			request.getRequestDispatcher("cars.html").forward(request, response);
+		}
+		else if(uri.endsWith("addcars"))// Each IF is for Request-Response Cycle
+		{
+			String[] selectedCars = request.getParameterValues("Car");
+			//Storing in Session as required in future.
+			session.setAttribute("selectedCars", selectedCars);
+			//Forward request to bikes
+			request.getRequestDispatcher("bikes.html").forward(request, response);
+		}
+		else if (uri.endsWith("addbikes"))
+		{
+			String[] selectedBikes = request.getParameterValues("Bike");
+			String[] selectedBooks =(String[]) session.getAttribute("selectedBooks");
+			String[] selectedCars  =(String[]) session.getAttribute("selectedCars");
+			
+			PrintWriter out = response.getWriter();
+			out.println("<html><body>");
+			out.println("<h4>Selected Books</h4>");
+			out.println("<ul>");
+			Stream.of(selectedBooks).map(s->"<li>"+s+"</li>").forEach(out::println);
+			out.println("</ul>"); 
+			out.println("<h4>Selected Cars</h4>");
+			out.println("<ul>");
+			Stream.of(selectedCars).map(s->"<li>"+s+"</li>").forEach(out::println);
+			out.println("</ul>");
+			out.println("<h4>Selected Bikes</h4>");
+			out.println("<ul>");
+			Stream.of(selectedBikes).map(s->"<li>"+s+"</li>").forEach(out::println);
+			out.println("</ul>");
+			out.println("</body></html>");
+			
+			session.invalidate();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ webapp/src/main/webapp/cars.html
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Select your favourite cars</title>
+</head>
+<body>
+	<form action="addcars" method="get">
+		Car 1<input type="checkbox" name="Car" value="Car 1">
+		Car 2<input type="checkbox" name="Car" value="Car 2">
+		Car 3<input type="checkbox" name="Car" value="Car 3">
+		Car 4<input type="checkbox" name="Car" value="Car 4">
+		
+		<input type="submit" value="NEXT">
+	</form>
+</body>
+</html>
